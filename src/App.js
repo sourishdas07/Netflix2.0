153b6285c682c6033fe85c1d9d1957fd83a0af02
sourishdas07 <109391867+sourishdas07@users.noreply.github.com>
2023-01-21 19:13:10 -0500

    start of loginscreen

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from "react";
 import "./App.css";
 import HomeScreen from "./HomeScreen";
 import { Route, Routes} from "react-router-dom";
+import LoginScreen from "./LoginScreen";
 
 function App() {
+
+  const user = {name: "sourish"}; //temp
+
+  // const user = null;
+
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<HomeScreen />} />
+        {!user ? (
+          <LoginScreen />
+        ): (
+        <Route path="/" element={<LoginScreen />} />
+      )}
       </Routes>
     </div>
   );
--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import './LoginScreen.css'
 
 function LoginScreen() {
   return (
-    <div>
-      
+    <div className="loginScreen">
+      <div className="loginScreen__background">
+        <img
+          className="loginScreen_logo"
+          src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+          alt=""
+        />
+        
+        <button className="loginScreen__button">Sign In</button>
+
+        <div className="loginScreen__gradient" />
+      </div>
     </div>
-  )
+  );
 }
 
 export default LoginScreen
